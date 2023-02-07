54a7214cd53ea0bba6e0d5139073598bbfeda02c
Rodrigo Jacinto <rodrigojacinto16@gmail.com>
2023-02-07 15:13:52 -0300

    feat:adicionando tsx nos componentes

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,17 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repository.scss";
 
+interface Repository {
+  id: string;
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
@@ -15,7 +23,7 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        {repositories.map((repoItem) => {
+        {repositories.map((repoItem: Repository) => {
           return <RepositoryItem key={repoItem.id} repository={repoItem} />;
         })}
       </ul>
