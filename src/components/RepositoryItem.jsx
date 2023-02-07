4f33643b1ac006d590effbb222e5cabb36e353b1
Rodrigo Jacinto <rodrigojacinto16@gmail.com>
2023-02-07 01:48:20 -0300

    feat: estilizando tela e recebendo dados do github

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem({repository}) {
       <strong>{repository.name}</strong>
       <p>{repository.description}</p>
 
-      <a href={repository}>Acessar repositório</a>
+      <a href={repository.html_url}>Acessar repositório</a>
     </li>
   );
 }
